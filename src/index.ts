42a5e2a82c28e3f42f7bfa41c803d5740f8ff067
UltimateDoge5 <piotr5kozak@gmail.com>
2021-12-23 13:29:40 +0100

    App now starts a minecraft server process
    App now starts a java minecraft server process from a file supplied in config.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
+import { spawn } from "child_process";
 import express, { Request, Response } from "express";
 import { join } from "path";
+
+const dotenv = require("dotenv");
+
+const result = dotenv.config({ path: join(__dirname, "../config.env") });
+
+if (result.error) {
+	throw result.error;
+}
 
 const app = express();
 
@@ -7,6 +16,19 @@ app.use(express.static(join(__dirname, "web/build")));
 
 app.get("*", (req_: Request, res: Response) => {
 	res.sendFile(join(__dirname + "/web/build/index.html"));
+});
+
+if (process.env.SERVER_JAR == undefined) {
+	throw "SERVER_JAR environment variable not set";
+} else if (process.env.SERVER_DIR == undefined) {
+	throw "SERVER_DIR environment variable not set";
+}
+
+console.log("Starting minecraft server...");
+const server = spawn("java", ["-jar", process.env.SERVER_JAR, "--nogui"], { cwd: process.env.SERVER_DIR });
+
+server.stdout.on("data", (data) => {
+	console.log(`stdout: ${data}`);
 });
 
 app.listen(8080, () => console.log("Listening on port 8080"));
