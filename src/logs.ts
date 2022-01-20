c7e91770697113fd59af0a118cfe5fee2f25570b
UltimateDoge5 <piotr5kozak@gmail.com>
2022-01-20 19:26:06 +0100

    Added the log viewer
    Added the log viewer page. It can be accessed by clicking one of the logs in the list.

--- src/logs.ts
+++ src/logs.ts
@@ -1,40 +1,41 @@
 import { Request, Response, Router } from "express";
-import { readdir, readFile, rename, stat, unlink, writeFile } from "fs/promises";
+import { readdir, readFile, rename, stat, unlink, writeFile, access } from "fs/promises";
 import { join } from "path";
 import { ungzip } from "node-gzip";
 
 const logsRouter = Router();
 
 logsRouter.get("/logs", async (req: Request, res: Response) => {
-	if (req.body.log != undefined) {
-		if ((await stat(join(process.env.SERVER_DIR as string, "logs", req.body.log + ".log.gz")), { throwIfNoEntry: false }) != undefined) {
-			const log = await ungzip(await readFile(join(process.env.SERVER_DIR as string, "logs", req.body.log + ".log.gz")));
+	const logsRaw = await readdir(join(process.env.SERVER_DIR as string, "logs"));
+	let logs: Log[] = [];
 
-			//Overwrite the .gz file with the uncompressed file to mantain the creation date.
-			await rename(
-				join(process.env.SERVER_DIR as string, "logs", req.body.log + ".log.gz"),
-				join(process.env.SERVER_DIR as string, "logs", req.body.log + ".log")
-			);
-			await writeFile(join(process.env.SERVER_DIR as string, "logs", req.body.log + ".log"), log, { encoding: "utf8" });
+	for (const log of logsRaw) {
+		const logsStats = await stat(join(process.env.SERVER_DIR as string, "logs", log));
+		logs.push({ name: log.replace(/(\.log)|(\.gz)/g, ""), creationDate: logsStats.birthtime.getTime() });
+	}
 
-			res.status(200).json({ log: log.toString() });
-		} else if ((await stat(join(process.env.SERVER_DIR as string, "logs", req.body.log + ".log")), { throwIfNoEntry: false }) != undefined) {
-			const log = await readFile(join(process.env.SERVER_DIR as string, "logs", req.body.log + ".log"), "utf8");
-			res.status(200).json({ log });
-		} else {
-			res.sendStatus(404);
-		}
+	res.json({ logs });
+});
+
+logsRouter.get("/log/:logID", async (req: Request, res: Response) => {
+	const logID = req.params.logID;
+
+	if (await exists(join(process.env.SERVER_DIR as string, "logs", logID + ".log.gz"))) {
+		const log = await ungzip(await readFile(join(process.env.SERVER_DIR as string, "logs", logID + ".log.gz")));
+
+		//Overwrite the .gz file with the uncompressed file to mantain the creation date.
+		await rename(join(process.env.SERVER_DIR as string, "logs", logID + ".log.gz"), join(process.env.SERVER_DIR as string, "logs", logID + ".log"));
+		await writeFile(join(process.env.SERVER_DIR as string, "logs", logID + ".log"), log, { encoding: "utf8" });
+
+		const { birthtime } = await stat(join(process.env.SERVER_DIR as string, "logs", logID + ".log"));
+
+		res.status(200).json({ log: log.toString(), creationDate: birthtime.getTime() });
+	} else if (await exists(join(process.env.SERVER_DIR as string, "logs", logID + ".log"))) {
+		const log = await readFile(join(process.env.SERVER_DIR as string, "logs", logID + ".log"), "utf8");
+		const { birthtime } = await stat(join(process.env.SERVER_DIR as string, "logs", logID + ".log"));
+		res.status(200).json({ log, creationDate: birthtime.getTime() });
 	} else {
-		const logsRaw = await readdir(join(process.env.SERVER_DIR as string, "logs"));
-
-		let logs: Log[] = [];
-
-		for (const log of logsRaw) {
-			const logsStats = await stat(join(process.env.SERVER_DIR as string, "logs", log));
-			logs.push({ name: log.replace(/(\.log)|(\.gz)/g, ""), creationDate: logsStats.birthtime.getTime() });
-		}
-
-		res.json({ logs });
+		res.sendStatus(404);
 	}
 });
 
@@ -42,9 +43,9 @@ logsRouter.delete("/logs", async (req: Request, res: Response) => {
 	const { log } = req.body;
 
 	if (log != undefined) {
-		if ((await stat(join(process.env.SERVER_DIR as string, "logs", log + ".log.gz")), { throwIfNoEntry: false }) != undefined) {
+		if (await exists(join(process.env.SERVER_DIR as string, "logs", log + ".log.gz"))) {
 			await unlink(join(process.env.SERVER_DIR as string, "logs", log + ".log.gz"));
-		} else if ((await stat(join(process.env.SERVER_DIR as string, "logs", log + ".log")), { throwIfNoEntry: false }) != undefined) {
+		} else if (await exists(join(process.env.SERVER_DIR as string, "logs", log + ".log"))) {
 			await unlink(join(process.env.SERVER_DIR as string, "logs", log + ".log"));
 		} else {
 			return res.sendStatus(404);
@@ -63,5 +64,14 @@ logsRouter.delete("/logs", async (req: Request, res: Response) => {
 		res.sendStatus(400);
 	}
 });
+
+const exists = async (path: string) => {
+	try {
+		await access(path);
+		return true;
+	} catch (e) {
+		return false;
+	}
+};
 
 export default logsRouter;
